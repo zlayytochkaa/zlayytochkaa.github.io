6e68ad2d632d91a96c6ee65f7139a99d43b643d1
zlay_ytochka <33456732+ZlayYTKA@users.noreply.github.com>
2024-07-30 08:02:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const urlParams = new URLSearchParams(window.location.search);
     const userId = urlParams.get('user_id');
 
-    fetch(`https://dfa9-31-129-105-188.ngrok-free.app/user/${userId}`)
-        .then(response => {
-            console.log(response);  // Отладка: выводим полный ответ
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();  // Отладка: используем text() для проверки
-        })
-        .then(data => {
-            console.log(data);  // Отладка: выводим сырые данные
-            try {
-                const parsedData = JSON.parse(data);  // Пробуем парсить JSON
-                const greeting = document.getElementById('greeting');
-                const name = parsedData.fullname || parsedData.username;
-                greeting.innerText += name;
-            } catch (e) {
-                console.error('Error parsing JSON:', e);
-            }
-        })
-        .catch(error => console.error('Error:', error));
+    fetch(`https://dfa9-31-129-105-188.ngrok-free.app/user/${userId}`, {
+        headers: {
+            'ngrok-skip-browser-warning': 'true'
+        }
+    })
+    .then(response => {
+        console.log(response);  // Отладка: выводим полный ответ
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);  // Отладка: выводим сырые данные
+        const greeting = document.getElementById('greeting');
+        const name = data.fullname || data.username;
+        greeting.innerText += name;
+    })
+    .catch(error => console.error('Error:', error));
 });
