67e711203f2123b08050745f4f8c142536780492
zlay_ytochka <33456732+ZlayYTKA@users.noreply.github.com>
2024-07-30 07:47:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const userId = urlParams.get('user_id');
 
     fetch(`https://dfa9-31-129-105-188.ngrok-free.app/user/${userId}`)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
             const greeting = document.getElementById('greeting');
             const name = data.fullname || data.username;
