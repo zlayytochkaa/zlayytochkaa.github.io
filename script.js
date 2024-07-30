cd2c96e9c4e39198353d11c97a238528154645a0
zlay_ytochka <33456732+ZlayYTKA@users.noreply.github.com>
2024-07-30 07:50:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const userId = urlParams.get('user_id');
 
     fetch(`https://dfa9-31-129-105-188.ngrok-free.app/user/${userId}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
+        .then(response => response.text())  // Use text() to see the raw response
         .then(data => {
+            console.log(data);  // Output the raw response to console
+            const parsedData = JSON.parse(data);
             const greeting = document.getElementById('greeting');
-            const name = data.fullname || data.username;
+            const name = parsedData.fullname || parsedData.username;
             greeting.innerText += name;
         })
         .catch(error => console.error('Error:', error));
